b32d9313f0b6a3e007c60cb269a9714dde7e8b2c
omaramgad1 <omaramgad89@gmail.com>
2022-12-20 22:43:57 +0200

    test 1

--- DBMS_Scripts/insert_into_table.sh
+++ DBMS_Scripts/insert_into_table.sh
@@ -1,51 +1,46 @@
 #!/bin/bash
 source ./db_root_path.sh
-current_db=`cat "$DB_PATH/current_db"`
+current_db=$(cat "$DB_PATH/current_db")
 declare table_name=""
 declare -a data_type
 declare -a col_names
 typeset -i counter=1
 typeset -i index=0
-if [ -z $current_db ]
-then
-    echo '+---------------------------------+'
+if [ -z $current_db ]; then
+	echo '+---------------------------------+'
 	echo "You are not connected to DB"
-    echo '+---------------------------------+'
+	echo '+---------------------------------+'
 	exit 1
 fi
 # get table name
 echo "Enter table name"
 read -r table_name
-while [ ! -f "$DB_PATH/$current_db/$table_name" ]
-do
-    echo '+---------------------------------+'
-    echo "Enter table name"
-    read -r table_name
-    echo '+---------------------------------+'
+while [ ! -f "$DB_PATH/$current_db/$table_name" ]; do
+	echo '+---------------------------------+'
+	echo "Enter table name"
+	read -r table_name
+	echo '+---------------------------------+'
 done
 
-
 #store data types & col_names
-type=`awk 'NR==1{print}' "$DB_PATH/$current_db/$table_name"  | cut -d '|' -f 1`
-col_name=`awk 'NR==2{print}' "$DB_PATH/$current_db/$table_name"  | cut -d '|' -f 1`
+type=$(awk 'NR==1{print}' "$DB_PATH/$current_db/$table_name" | cut -d '|' -f 1)
+col_name=$(awk 'NR==2{print}' "$DB_PATH/$current_db/$table_name" | cut -d '|' -f 1)
 
-if [ -z $type ]
-then
-    echo '+---------------------------------+'
-    echo "This table has no structure!"
-    echo '+---------------------------------+'
-    exit 1
+if [ -z $type ]; then
+	echo '+---------------------------------+'
+	echo "This table has no structure!"
+	echo '+---------------------------------+'
+	exit 1
 fi
-while [ ! -z "$type" ]
-do
-    counter+=$((1))
+while [ ! -z "$type" ]; do
+	counter+=$((1))
 	data_type[$index]=$type
-    col_names[$index]=$col_name
+	col_names[$index]=$col_name
 
 	index+=$((1))
 
-    col_name=`awk 'NR==2{print}' "$DB_PATH/$current_db/$table_name"  | cut -d '|' -f $counter`
-    type=`awk 'NR==1{print}' "$DB_PATH/$current_db/$table_name"  | cut -d '|' -f $counter`
+	col_name=$(awk 'NR==2{print}' "$DB_PATH/$current_db/$table_name" | cut -d '|' -f $counter)
+	type=$(awk 'NR==1{print}' "$DB_PATH/$current_db/$table_name" | cut -d '|' -f $counter)
 
 done
 
@@ -57,23 +52,18 @@ data=""
 counter=1
 dublicate=0
 typeset -a row
-while [[ $index < $col_no ]]
-do
-    	echo "column type: ${data_type[$index]} column name: ${col_names[$index]}";
+while [[ $index < $col_no ]]; do
+	echo "column type: ${data_type[$index]} column name: ${col_names[$index]}"
 	read -r data
 
-	# this column is PK	
-	if [[ ${data_type[$index]} == *":"* ]]  
-	then
-		if [ -z $data ]
-		then
+	# this column is PK
+	if [[ ${data_type[$index]} == *":"* ]]; then
+		if [ -z $data ]; then
 			echo "This column can not be empty"
 			continue
-		fi	
-		for x in `cat "$DB_PATH/$current_db/$table_name" | cut -d '|' -f $counter`
-		do
-	 		if [[ $data == $x ]]
-			then
+		fi
+		for x in $(cat "$DB_PATH/$current_db/$table_name" | cut -d '|' -f $counter); do
+			if [[ $data == $x ]]; then
 				echo '+---------------------------------+'
 				echo "PK must be unique"
 				echo '+---------------------------------+'
@@ -81,39 +71,35 @@ do
 				break
 			fi
 		done
-		if [[ $dublicate == 1 ]]
-		then 
+		if [[ $dublicate == 1 ]]; then
 			dublicate=0
 			continue
 		fi
-		
+
 	fi
-	
-	
+
 	# check for integer
-	if [[ ${data_type[$index]} == *"int"* && $data =~ ^-?[0-9]+$ ]]
-	then 
+	if [[ ${data_type[$index]} == *"int"* && $data =~ ^-?[0-9]+$ ]]; then
 		row[$index]="$data|"
-	elif [[ ${data_type[$index]} == *"str"* && $data =~ ^[A-Za-z].* ]]
-	then
+	elif [[ ${data_type[$index]} == *"str"* && $data =~ ^[A-Za-z].* ]]; then
 		row[$index]="$data|"
-	elif [ -z $data ]
-    then
-        row[$index]="$data|"
-    else
+	elif [ -z $data ]; then
+		row[$index]="$data|"
+	elif [ -z $data ]; then
+		row[$index]="$data|"
+	else
 		echo '+---------------------------------+'
 		echo "PLease enter valid data"
 		echo '+---------------------------------+'
 		continue
-	fi	
-		(( counter += 1))
-    	(( index += 1 ))
+	fi
+	((counter += 1))
+	((index += 1))
 done
 
 echo '+-----------------------------------------------+'
-for x in "${row[@]}"
-do
-	echo -e "$x\c" >> "$DB_PATH/$current_db/$table_name"
+for x in "${row[@]}"; do
+	echo -e "$x\c" >>"$DB_PATH/$current_db/$table_name"
 done
-echo "" >> "$DB_PATH/$current_db/$table_name"
+echo "" >>"$DB_PATH/$current_db/$table_name"
 echo '+-----------------------------------------------+'
